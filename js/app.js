9b93a588e62d56999834709c38e256fb5b28fabd
Tanaka583 <karleetanaka@student.midpac.edu>
2016-03-25 14:15:22 -1000

    Added More Content
    Added More Content

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+
+function example1() {
+	document.getElementById('cssExample').innerHTML = "<style>#cssExample{font-family:courier;color: #20B2AA;background-color:red;}</style>Testing this style."
+}
+
+function example2() {
+	document.getElementById('cssExample').innerHTML = "<style>#cssExample{font-family:arial;color:white;background-color:black;}</style>Testing this style."
+}
