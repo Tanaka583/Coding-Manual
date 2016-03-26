f9809584a14313381ce3ef0cdb1bb029a5e25d1e
Tanaka583 <karleetanaka@student.midpac.edu>
2016-03-25 14:59:06 -1000

    The New Changes
    The new changes. :)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 
 
 function example1() {
-	document.getElementById('cssExample').innerHTML = "<style>#cssExample{font-family:courier;color: #20B2AA;background-color:red;}</style>Testing this style."
+	document.getElementById('cssExample').innerHTML = "<style>#cssExample{font-family:sans-serief;color: black;background-color:red;}</style>Isn't this cool?!";
 }
 
 function example2() {
-	document.getElementById('cssExample').innerHTML = "<style>#cssExample{font-family:arial;color:white;background-color:black;}</style>Testing this style."
+  	document.getElementById('cssExample').innerHTML = "<style>#cssExample{font-family:ariel;color: midnight-blue;background-color:white;}</style>Isn't this cool?!";
+}
+
+function example3() {
+    document.getElementById('cssExample').innerHTML = "<style>#cssExample{font-family:serief;color: magenta;background-color:yellow;}</style>Isn't this cool?!";
+}
+
+function example4() {
+	document.getElementById('cssExample').innerHTML = "<style>#cssExample{font-family:times;color: white;background-color:green;}</style>Isn't this cool?!";
 }
